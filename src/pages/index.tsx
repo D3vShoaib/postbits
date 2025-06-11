1a2f790f9e34dbedb1cac2743ab137c8b64bab16
d3vshoaib <shoaib.khan200499@gmail.com>
2025-06-11 13:28:58 +0530

    used te right fade conponent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,69 +3,46 @@ import Aurora from "@/components/ui/Aurora";
 import { Input } from "@heroui/input";
 import { LinkIcon } from "@/components/icons";
 import LinksTable from "@/components/home/LinksTable";
-import BlurReveal from "../components/ui/reactbits/blur-reveal/BlurReveal";
-
+import FadeContent from "@/components/ui/reactbits/FadeContent";
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <div className="relative z-10 flex flex-col items-center justify-center  text-center overflow-hidden">
-        <div className="w-full max-w-2xl flex flex-col gap-5 items-center justify-center">
-          <h1 className="text-4xl md:text-6xl leading-tight text-white">
-            <BlurReveal
-              baseOpacity={0}
-              enableBlur={true}
-              baseRotation={5}
-              blurStrength={10}
-              staggerAmount={0.1}
-            >
-              Any Link to a click
-            </BlurReveal>
-            <br />
-            <span className="font-serif font-bold  italic text-[#7fa8ae]">
-              <BlurReveal
-                baseOpacity={0}
-                enableBlur={true}
-                baseRotation={5}
-                blurStrength={10}
-                staggerAmount={0.4}
-              >
-                worthy{" "}
-              </BlurReveal>
-            </span>
-            <BlurReveal
-              baseOpacity={0}
-              enableBlur={true}
-              baseRotation={5}
-              blurStrength={10}
-              staggerAmount={0.6}
-            >
-              Post Instantly.
-            </BlurReveal>
-          </h1>
-          <p className="mt-4 text-lg md:text-xl text-gray-300 font-light">
-            <BlurReveal
-              baseOpacity={0}
-              enableBlur={true}
-              baseRotation={5}
-              blurStrength={10}
-              wordAnimationEnd="bottom bottom-=10%"
-              staggerAmount={0.05}
-            >
-              Paste a link, get a crisp post with an image tailored for you.
-            </BlurReveal>
-          </p>
-          <Input
-            placeholder="Enter your link"
-            startContent={<LinkIcon className="text-default-400" />}
-            variant="bordered"
-            className="w-full"
-          />
-          <div className="w-full">
-            <LinksTable />
+      <FadeContent
+        blur={true}
+        duration={1000}
+        easing="ease-out"
+        initialOpacity={0}
+      >
+        <div className="relative z-10 flex flex-col items-center justify-center  text-center overflow-hidden">
+          <div className="w-full max-w-2xl flex flex-col gap-5 items-center justify-center">
+            <div>
+              <h1 className="text-4xl md:text-6xl leading-tight text-white">
+                Any Link to a click
+                <br />
+                <div className="mt-0 md:mt-4 ">
+                  <span className="font-serif font-bold italic text-[#7fa8ae]">
+                    worthy{" "}
+                  </span>
+                  Post Instantly.
+                </div>
+              </h1>
+              <p className="mt-4 text-lg md:text-xl text-gray-300 font-light">
+                Paste a link, get a crisp post with an image tailored for you.
+              </p>
+            </div>
+
+            <Input
+              placeholder="Enter your link"
+              startContent={<LinkIcon className="text-default-400" />}
+              variant="bordered"
+              className="w-full"
+            />
+            <div className="w-full">
+              <LinksTable />
+            </div>
           </div>
         </div>
-      </div>
-
+      </FadeContent>
       <div className="absolute inset-0 -z-10 opacity-60">
         <div className="absolute inset-0 backdrop-blur-[40px]"></div>
         <Aurora
